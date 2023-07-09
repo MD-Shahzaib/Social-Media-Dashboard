d56f7ef567a1befe75f4bc6c9ed42f63363e1f85
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-07-09 01:56:09 -0700

    Create simple UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MetricsDisplay from './components/MetricsDisplay';
 import Charts from './components/Charts';
 import UserInformation from './components/UserInformation';
 import mockData from './components/mockData';
 
 const App = () => {
+  const [metrics, setMetrics] = useState({});
+  const [chartData, setChartData] = useState({});
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetchMetrics();
+    fetchChartData();
+    fetchUser();
+  }, []);
+
+  const fetchMetrics = async () => {
+    try {
+      // Simulating API request delay
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setMetrics(mockData.metrics);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchChartData = async () => {
+    try {
+      // Simulating API request delay
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setChartData(mockData.chartData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchUser = async () => {
+    try {
+      // Simulating API request delay
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setUser(mockData.user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold mb-4">Social Media Dashboard</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <MetricsDisplay {...mockData.metrics} />
-        <Charts data={mockData.chartData} />
-        <UserInformation {...mockData.user} />
+    <div className="bg-gray-900 text-white min-h-screen py-8">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold mb-8 text-center">Social Media Dashboard</h1>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div className="bg-gray-800 rounded-lg p-6 shadow-lg">
+            <MetricsDisplay {...metrics} />
+          </div>
+          <div className="bg-gray-800 rounded-lg p-6 shadow-lg">
+            <Charts data={chartData} />
+          </div>
+          <div className="bg-gray-800 rounded-lg p-6 shadow-lg">
+            <UserInformation {...user} />
+          </div>
+          <div className="bg-gray-800 rounded-lg shadow-lg">
+            {/* Additional Component */}
+            <div className="p-6">
+              <h2 className="text-lg font-semibold mb-4">Engagement Rate</h2>
+              <div className="flex items-center">
+                <div className="w-16 h-16 bg-blue-500 rounded-full flex-shrink-0">
+                  <div className="flex items-center justify-center h-full">
+                    <span className="text-white text-2xl">{metrics.engagementRate}</span>
+                  </div>
+                </div>
+                <div className="ml-4">
+                  <p className="text-gray-500">Engagement Rate</p>
+                  <p className="font-semibold">{metrics.totalLikes} Likes | {metrics.totalComments} Comments</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,101 +1,46 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-
+import React, { useEffect, useRef, useState } from 'react';
+import Chart from 'chart.js/auto';
 
 const Charts = ({ data }) => {
-    const chartData = {
-        labels: data.labels,
-        datasets: [
-            {
-                label: 'Metrics',
-                data: data.values,
-                backgroundColor: 'rgba(75, 192, 192, 0.6)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1,
-            },
-        ],
-    };
+    const [chartInstance, setChartInstance] = useState(null);
+    const chartRef = useRef(null);
 
-    const chartOptions = {
-        scales: {
-            y: {
-                beginAtZero: true,
+    useEffect(() => {
+        if (chartRef.current) {
+            chartRef.current.destroy();
+        }
+
+        const ctx = document.getElementById('chart');
+
+        const newChartInstance = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
             },
-        },
-    };
+        });
+
+        setChartInstance(newChartInstance);
+        chartRef.current = newChartInstance;
+
+        return () => {
+            if (chartInstance) {
+                chartInstance.destroy();
+            }
+        };
+    }, [data]);
 
     return (
         <div>
             <h2>Charts</h2>
-            <Bar data={chartData} options={chartOptions} />
+            <canvas id="chart" />
         </div>
     );
 };
 
 export default Charts;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Bar, Chart, destroy } from 'react-chartjs-2';
-
-// const Charts = ({ data }) => {
-//     const chartData = {
-//         labels: data.labels,
-//         datasets: [
-//             {
-//                 label: 'Metrics',
-//                 data: data.values,
-//                 backgroundColor: 'rgba(75, 192, 192, 0.6)',
-//                 borderColor: 'rgba(75, 192, 192, 1)',
-//                 borderWidth: 1,
-//             },
-//         ],
-//     };
-
-//     const chartOptions = {
-//         scales: {
-//             y: {
-//                 beginAtZero: true,
-//             },
-//         },
-//     };
-
-//     useEffect(() => {
-//         if (chartInstance) {
-//             destroy(chartInstance); // Destroy the previous chart instance
-//         }
-
-//         const newChartInstance = new Chart(document.getElementById('chart'), {
-//             type: 'bar',
-//             data: chartData,
-//             options: chartOptions,
-//         });
-
-//         setChartInstance(newChartInstance);
-
-//         return () => {
-//             if (chartInstance) {
-//                 destroy(chartInstance); // Destroy the chart instance on component unmount
-//             }
-//         };
-//     }, [chartData, chartOptions]);
-
-//     return (
-//         <div>
-//             <h2>Charts</h2>
-//             <canvas id="chart" />
-//         </div>
-//     );
-// };
